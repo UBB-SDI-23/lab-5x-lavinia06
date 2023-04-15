1ccfb8ae86e24b847afc40b51341e53ccb6242aa
914_Ionel_Lavinia <67369509+lavinia06@users.noreply.github.com>
2023-04-15 16:01:04 +0300

    frontend

--- dress_frontend/src/constants.ts
+++ dress_frontend/src/constants.ts
@@ -1,6 +1,9 @@
-//const PROD_BACKEND_API_URL = "/";
+const PROD_BACKEND_API_URL = "/api";
 
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
 
-export const BACKEND_API_URL = "http://127.0.0.1:8000/api";
-//fetch(BACKEND_API_URL, {mode: "no-cors"})
+//export const BACKEND_API_URL = "http://127.0.0.1:8000/api";
+
+export const BACKEND_API_URL =
+
+    process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
