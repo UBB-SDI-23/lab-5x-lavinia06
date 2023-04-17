fd0be8fba25ed93756897bef917f75b100aea819
914_Ionel_Lavinia <67369509+lavinia06@users.noreply.github.com>
2023-04-17 17:26:40 +0300

    vm

--- dress_frontend/src/constants.ts
+++ dress_frontend/src/constants.ts
@@ -1,8 +1,11 @@
 const PROD_BACKEND_API_URL = "http://127.0.0.1:8000/api";
-
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
 
-//export const BACKEND_API_URL = "http://127.0.0.1:8000/api";
+
+
+// const PROD_BACKEND_API_URL = "/api";
+// const DEV_BACKEND_API_URL = "http://34.155.183.149/";
+
 
 export const BACKEND_API_URL =
 
